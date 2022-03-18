236adf44f700c4e2be5418cf3ecbea24daa48352
Ishwari Rauniyar <ishirauniyar@gmail.com>
2022-03-18 16:01:30 +0545

    client login page

---
+++ client/src/pages/Login.js
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function loginUser(event) {
+    event.preventDefault();
+
+    const response = await fetch("http://localhost:8848/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+    console.log("resp", response);
+    console.log("body", response.body);
+
+    const data = await response.json();
+    console.log("data", data);
+
+    if (data.token) {
+      localStorage.setItem("token", data.token);
+      alert("Login successful");
+      window.location.href = "/dashboard";
+    } else {
+      alert("Please check your username and password");
+    }
+  }
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={loginUser}>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="Email"
+        />
+        <br />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="Password"
+        />
+        <br />
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
+}
+
+export default Login;

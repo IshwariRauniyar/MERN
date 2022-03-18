c871f4b703111fd8a08956f5754970bd01a49bca
Ishwari Rauniyar <ishirauniyar@gmail.com>
2022-03-18 16:00:47 +0545

    cors added

--- server/index.js
+++ server/index.js
@@ -3,7 +3,9 @@ const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const routes = require("./routes");
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 
 mongoose.connect("mongodb://localhost:27017/post");
